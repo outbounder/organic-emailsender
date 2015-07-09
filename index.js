61150d57cb7f51dc09af895d50ee6ca90d8a200c
aleksey-gonchar <gonchar.aleksey@gmail.com>
2015-07-09 16:12:35 +0700

    + emailTmpl logic & promises

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var fs = require("fs")
 var path = require("path")
 var _ = require("underscore")
 var async = require("async")
+var R = require('ramda')
+var Promise = require('bluebird')
 
 var pathIsFull = function(value){
   return value.indexOf("/") === 0 || value.indexOf(":\\")===1
@@ -40,28 +42,30 @@ module.exports = function(plasma, dna){
 
   if(dna.log && this.transport)
     console.log("sending emails using", dna.email.transport, dna.email.options)
-  
+
   var self = this
   plasma.on(dna.reactOn || "sendEmail", function(c, next){
-    self.sendEmail(c, next)
+    self.sendEmail(c)
+      .then(function (res) { next(null, res) })
+      .catch(function (err) { next(err) })
   })
 }
 
-module.exports.prototype.sendEmail = function(options, next){
+module.exports.prototype.sendEmail = function (options) {
+  var deferred = Promise.defer()
   var self = this
-  this.loadTemplate(options, function(err, template){
-    if(err) return next(err)
-  
+
+  function checkOptions(template) {
     var subject = options.subject
 
-    if(self.dna.i18next) {
+    if (self.dna.i18next) {
       var i18n = require('i18next')
       i18n.init(self.dna.i18next)
-      if(options.locale || self.dna.locale)
+      if (options.locale || self.dna.locale)
         i18n.setLng(options.locale || self.dna.locale)
-      
-      if(options.subject)
-        if(options.subject.key)
+
+      if (options.subject)
+        if (options.subject.key)
           subject = i18n.t(options.subject.key, options.subject.data)
         else
           subject = i18n.t(options.subject)
@@ -70,48 +74,102 @@ module.exports.prototype.sendEmail = function(options, next){
       options.data.t = i18n.t
     }
 
+    return options
+  }
+
+  function renderTmpl(options) {
+    var deferred = Promise.defer()
+    if (template.render) {
+      template.render(options.data, function whenRendered (err, results) {
+        deferred.resolve(results.html)
+      })
+    } else
+      deferred.resolve(template(options.data))
+
+    return deferred.promise
+  }
+
+  function send(html) {
+    var deferred = Promise.defer()
+
     var sendMailOptions = _.extend({
       from: options.from || self.dna.from,
       to: options.to || self.dna.to,
       subject: subject,
-      html: template(options.data)
+      html: html
     }, options.sendMailOptions || {})
 
     if(self.transport)
-      self.transport.sendMail(sendMailOptions, next)
+      self.transport.sendMail(sendMailOptions, function (err, res) {
+        if (err) { deferred.reject(err) }
+        deferred.resolve(res)
+      })
     else
-      next(null, sendMailOptions)
-  })   
+      deferred.resolve(sendMailOptions)
+
+    return deferred.promise
+  }
+
+  return this
+    .loadTemplate(options)
+    .then(checkOptions)
+    .then(renderTmpl)
+    .then(send)
 }
 
-module.exports.prototype.loadTemplate = function(options, next) {
+module.exports.prototype.loadTemplate = function(options) {
+  var deferred = Promise.defer()
+
   var locale = options.locale || this.dna.locale
   var template = this.templateCache[options.template+"-"+locale]
-  if(template) 
-    return next(null, template)
+
+  if(template)
+    return Promise.resolve(template)
+
   var root = ""
   if(this.dna.root && !pathIsFull(this.dna.root))
     root = path.join(process.cwd(), this.dna.root)
-  else
-  if(this.dna.root)
+  else if(this.dna.root)
     root = this.dna.root
-  var localizedTemplate = path.join(root, options.template+"-"+locale+".jade")
-  var indexTemplate = path.join(root, options.template+".jade")
-  var templateTargets = [localizedTemplate, indexTemplate]
+
+  var localizedTemplate = path.join(root, options.template + "-" + locale + ".jade")
+  var indexTemplate = path.join(root, options.template + ".jade")
+  var folderTemplate = path.join(root, options.template)
+  var templateTargets = [localizedTemplate, indexTemplate, folderTemplate]
   var self = this
-  async.detect(templateTargets, fs.exists, function(found){
-    if(found) {
-      fs.readFile(found, function(err, fileData){
-        if(err) return next(err)
-        template = jade.compile(fileData, {
-          filename: found,
-          debug: self.dna.debug
+
+  function isFileOrDir (filePath, cb) {
+    fs.stat(filePath, function (err, stats) {
+      if (err) return cb(false)
+
+      if (stats.isFile() || stats.isDirectory())
+        cb(stats)
+      else
+        cb(false)
+    })
+  }
+
+  async.detect(templateTargets, isFileOrDir, function whenExisted(fileOrDir) {
+    fs.stat(fileOrDir, function whenGotStats(err, stats) {
+      if(stats.isFile()) {
+        fs.readFile(fileOrDir, function whenReadFile(err, fileData) {
+          if (err) return next(err)
+          template = jade.compile(fileData, {
+            filename: fileOrDir,
+            debug: self.dna.debug
+          })
+          if (self.dna.cache)
+            self.templateCache[options.template + "-" + locale] = template
+          next(null, template)
         })
-        if(self.dna.cache)
-          self.templateCache[options.template+"-"+locale] = template
+      } else if(stats.isDirectory()) {
+        var EmailTemplate = require('email-templates').EmailTemplate
+        template = new EmailTemplate(fileOrDir)
         next(null, template)
-      })
-    } else
-      next(new Error("couldn't find none of "+ templateTargets.join(" || ")))
+      } else
+        next(new Error("couldn't find none of "+ templateTargets.join(" || ")))
+    })
   })
+
+  return deferred.promise
 }
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,36 +1,36 @@
-describe("index", function(){
+describe("index", function () {
   var Plasma = require("organic-plasma")
   var EmailSender = require("../index")
 
-  it("sends email", function(next){
+  it("sends email", function (next) {
     var instance = new EmailSender(new Plasma(), {
       from: "me",
-      root: __dirname+"/data"
+      root: __dirname + "/data"
     })
     instance.sendEmail({
       template: "email",
       to: "somebody",
       subject: "test"
-    }, function(err, options){
+    }, function (err, options) {
       expect(options.from).toBe("me")
       expect(options.to).toBe("somebody")
       expect(options.html).toContain("sample")
       next()
     })
   })
-  
-  it("sends email via plasma", function(next){
+
+  it("sends email via plasma", function (next) {
     var plasma = new Plasma()
     var instance = new EmailSender(plasma, {
       from: "me",
-      root: __dirname+"/data"
+      root: __dirname + "/data"
     })
     plasma.emit({
       type: "sendEmail",
       template: "email",
       to: "somebody",
       subject: "test"
-    }, function(err, options){
+    }, function (err, options) {
       expect(options.from).toBe("me")
       expect(options.to).toBe("somebody")
       expect(options.html).toContain("sample")
